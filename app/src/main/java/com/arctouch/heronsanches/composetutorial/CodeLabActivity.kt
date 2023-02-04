69db53a2d02d921612c7eae2dc886c40e640ee81
Heron Sanches <heron.sanches@arctouch.com>
2023-02-03 23:38:11 -0300

    Animating your list

--- app/src/main/java/com/arctouch/heronsanches/composetutorial/CodeLabActivity.kt
+++ app/src/main/java/com/arctouch/heronsanches/composetutorial/CodeLabActivity.kt
@@ -3,6 +3,9 @@ package com.arctouch.heronsanches.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,6 +15,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -20,11 +25,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import com.arctouch.heronsanches.composetutorial.ui.theme.ComposeTutorialTheme
 
@@ -42,7 +48,7 @@ class CodeLabActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Surface(modifier = modifier) {
         if (shouldShowOnboarding) OnboardingScreen { shouldShowOnboarding = false }
@@ -53,18 +59,18 @@ fun MyApp(modifier: Modifier = Modifier) {
 @Composable
 fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("Android", "Compose", "Kotlin")
+    names: List<String> = List(1000) { "$it" }
 ) {
     Surface(modifier = modifier) {
-        Column(modifier = Modifier.padding(8.dp)) {
-            names.forEachIndexed { index, name ->
+        LazyColumn(modifier = Modifier.padding(8.dp)) {
+            itemsIndexed(items = names) { idx, name ->
                 /* TODO: understand it:
                     "Composable functions can execute frequently and in any order,
                         you must not rely on the ordering in which the code is executed,
                         or on how many times this function will be recomposed."
                 */
                 Greeting(name = name, modifier = Modifier.fillMaxWidth())
-                val isLastElement = index == names.size - 1
+                val isLastElement = idx == names.size - 1
                 if (!isLastElement) Spacer(modifier = Modifier.height(8.dp))
             }
         }
@@ -73,8 +79,16 @@ fun Greetings(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded) 48.dp else 0.dp,
+
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colorScheme.primary,
@@ -84,15 +98,15 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
             Spacer(modifier = Modifier.width(4.dp))
 
-            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(text = if (expanded.value) "Show less" else "Show more")
+            ElevatedButton(onClick = { expanded = !expanded }) {
+                Text(text = if (expanded) "Show less" else "Show more")
             }
         }
     }
